43491a412ff7f5117a0425f616a3d74cd97cb27b
Liam Ashenhurst <liamashenhurst@gmail.com>
2020-07-23 00:34:28 +0100

    corrected swagger to work with custom exception handler

--- src/main/java/com/spring/Euler/configuration/SpringFoxConfig.java
+++ src/main/java/com/spring/Euler/configuration/SpringFoxConfig.java
@@ -3,6 +3,8 @@ package com.spring.Euler.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.Tag;
@@ -12,7 +14,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SpringFoxConfig {
+public class SpringFoxConfig extends WebMvcConfigurationSupport {
     private final Tag answersTag = new Tag("Answers", "This controller calculates and returns answers to Project Euler problems.");
     private final Tag problemsTag = new Tag("Problems", "This controller returns concise descriptions of Project Euler problems for which this server contains the solution.");
     private final Tag functionsTag = new Tag("Functions", "This controller provides access to some of the functions used to calculate Project Euler answers.");
@@ -27,5 +29,13 @@ public class SpringFoxConfig {
                 .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
                 .build();
+    }
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
